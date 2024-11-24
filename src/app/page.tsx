762ad7411cee0aea3d39d525fdba42ac928e2059
wDeloo <wdelo.hack@gmail.com>
2024-11-24 19:24:44 +0100

    added board component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import Board from "@/components/board";
+
+const BOARD_SIZE = 8
+const BOARD_COLORS = ["whiteBoard", "blackBoard"]
+
 export default function Home() {
   return (
     <>
+      <main>
+        <Board boardSize={BOARD_SIZE} boardColors={BOARD_COLORS} />
+      </main>
     </>
   )
 }
---
+++ src/components/board.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+function isEven(num: number) {
+    return (num % 2) == 0
+}
+
+export default function Board(props: { boardSize: number, boardColors: string[] }) {
+    const { boardSize, boardColors } = props
+
+    return (
+        <div className={`m-10 grid grid-cols-2 w-fit h-fit rounded-[.25rem] overflow-hidden`} style={{gridTemplateColumns: `repeat(${boardSize}, minmax(0, 1fr))`}}>
+            {
+                (() => {
+                    const squares: JSX.Element[] = []
+                    for (let row = 0; row < boardSize; row++) {
+                        for (let square = 0; square < boardSize; square++) {
+                            let bgColor
+                            if (isEven(row)) {
+                                if (isEven(square)) {
+                                    bgColor = boardColors[0]
+                                } else {
+                                    bgColor = boardColors[1]
+                                }
+                            } else {
+                                if (isEven(square)) {
+                                    bgColor = boardColors[1]
+                                } else {
+                                    bgColor = boardColors[0]
+                                }
+                            }
+
+                            squares.push(<div key={`${row}-${square}`} className={`bg-${bgColor} h-24 w-24`} />)
+                        }
+                    }
+                    return squares
+                })()
+            }
+        </div>
+    )
+}
