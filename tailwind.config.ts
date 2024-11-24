eac8173ef2ce7febc30e739b03690f261cb419d8
wDeloo <wdelo.hack@gmail.com>
2024-11-24 18:38:46 +0100

    project created

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        blackBoard: "var(--blackBoard)",
+        whiteBoard: "var(--whiteBoard)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
