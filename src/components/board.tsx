7298616a7b58c7a5f87486d71d10233f70f0e4f7
wDeloo <wdelo.hack@gmail.com>
2024-11-25 20:50:58 +0100

    Added numbers and letters to board

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -4,6 +4,16 @@ function isEven(num: number) {
     return (num % 2) == 0
 }
 
+function getRowNumber(num: number, boardSize: number) {
+    return (boardSize + 1) - (num + 1)
+}
+
+function getColumnLetter(num: number, boardSize: number) {
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+    return letters[num]
+}
+
 export default function Board(props: { boardSize: number, boardColors: string[] }) {
     const { boardSize, boardColors } = props
 
@@ -13,24 +23,39 @@ export default function Board(props: { boardSize: number, boardColors: string[] 
                 (() => {
                     const squares: JSX.Element[] = []
                     for (let row = 0; row < boardSize; row++) {
-                        for (let square = 0; square < boardSize; square++) {
-                            let bgColor
+                        for (let column = 0; column < boardSize; column++) {
+                            const squareId = [getColumnLetter(column, boardSize), getRowNumber(row, boardSize)]
+
+                            let bgColor, guideColor
                             if (isEven(row)) {
-                                if (isEven(square)) {
+                                if (isEven(column)) {
                                     bgColor = boardColors[0]
+                                    guideColor = boardColors[1]
                                 } else {
                                     bgColor = boardColors[1]
+                                    guideColor = boardColors[0]
                                 }
                             } else {
-                                if (isEven(square)) {
+                                if (isEven(column)) {
                                     bgColor = boardColors[1]
+                                    guideColor = boardColors[0]
                                 } else {
                                     bgColor = boardColors[0]
+                                    guideColor = boardColors[1]
                                 }
                             }
 
+                            // text-whiteBoard / text-blackBoard
+                            let squareNumGuide, squareLetterGuide
+                            if (row === boardSize - 1) {
+                                squareLetterGuide = <span className={`absolute bottom-0 right-2 text-${guideColor}`}>{squareId[0]}</span>
+                            }
+                            if (column === 0) {
+                                squareNumGuide = <span className={`absolute top-0 left-1 text-${guideColor}`}>{squareId[1]}</span>
+                            }
+
                             // bg-whiteBoard / bg-blackBoard
-                            squares.push(<div key={`${row}-${square}`} className={`bg-${bgColor} h-24 w-24 select-none`} />)
+                            squares.push(<div key={`${row}-${column}`} className={`bg-${bgColor} h-24 w-24 text-xl font-bold select-none relative`}>{squareNumGuide}{squareLetterGuide}</div>)
                         }
                     }
                     return squares
