bce454f1164df513b7640bf13408d84ae8994afd
wDeloo <wdelo.hack@gmail.com>
2024-11-26 20:32:34 +0100

    removed 1v1 and analize links from nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -13,8 +13,6 @@ export default function Nav() {
         <nav className="pt-1 pb-6 h-screen w-fit bg-backgroundBox flex flex-col justify-between">
             <div className="flex flex-col font-bold *:p-3 *:transition-colors hover:*:bg-backgroundBoxHover text-lg hover:*:text-foregroundHighlighted">
                 <div className="flex flex-row gap-2"><Image height={30} width={30} alt="logo" src="images/logo.svg" /><a href="/">Brilliant Chess</a></div>
-                <a href="/analize">Analyze</a>
-                <a href="/play">1v1</a>
             </div>
             <div className="flex flex-col *:px-3 *:py-2 *:transition-colors hover:*:bg-backgroundBoxHover text-sm *:text-foregroundGrey hover:*:text-foregroundHighlighted font-bold">
                 <a href="#" onMouseEnter={() => setSupportHover(true)} onMouseLeave={() => setSupportHover(false)} className="flex flex-row gap-2"><Heart class={`${supportHover ? "fill-foregroundHighlighted" : "fill-foregroundGrey"} transition-colors`} />Support Me</a>
