a37472fd9e9f1683a0b70d89046dd35fe5d652ac
wDeloo <wdelo.hack@gmail.com>
2024-11-26 22:11:39 +0100

    board size (px) can be set from a prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Board from "@/components/board"
+import Menu from "@/components/menu"
 
 const BOARD_SIZE = 8
 const BOARD_COLORS = ["whiteBoard", "blackBoard"]
 
 export default function Home() {
   return (
-      <main>
-        <Board boardSize={BOARD_SIZE} boardColors={BOARD_COLORS} />
+      <main className="flex flex-row">
+        <Board boardProportions={BOARD_SIZE} boardColors={BOARD_COLORS} boardSize={750} />
+        <Menu />
       </main>
   )
 }
--- src/components/board.tsx
+++ src/components/board.tsx
@@ -4,27 +4,32 @@ function isEven(num: number) {
     return (num % 2) === 0
 }
 
-function getRowNumber(num: number, boardSize: number) {
-    return (boardSize + 1) - (num + 1)
+function getRowNumber(num: number, boardProportions: number) {
+    return (boardProportions + 1) - (num + 1)
 }
 
-function getColumnLetter(num: number, boardSize: number) {
+function getColumnLetter(num: number, boardProportions: number) {
     const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
     return letters[num]
 }
 
-export default function Board(props: { boardSize: number, boardColors: string[] }) {
-    const { boardSize, boardColors } = props
+export default function Board(props: { boardProportions: number, boardColors: string[], boardSize: number }) {
+    const { boardProportions, boardColors, boardSize } = props
+
+    const squareSize = boardSize / boardProportions
+    const guideSize = squareSize / 4
+    const leftSize = guideSize / 4.5
+    const rightSize = guideSize / 2.5
 
     return (
-        <div className="grid grid-cols-2 w-fit h-fit rounded-borderRoundness overflow-hidden" style={{gridTemplateColumns: `repeat(${boardSize}, minmax(0, 1fr))`}}>
+        <div className="grid grid-cols-2 w-fit h-fit rounded-borderRoundness overflow-hidden" style={{gridTemplateColumns: `repeat(${boardProportions}, minmax(0, 1fr))`}}>
             {
                 (() => {
                     const squares: JSX.Element[] = []
-                    for (let row = 0; row < boardSize; row++) {
-                        for (let column = 0; column < boardSize; column++) {
-                            const squareId = [getColumnLetter(column, boardSize), getRowNumber(row, boardSize)]
+                    for (let row = 0; row < boardProportions; row++) {
+                        for (let column = 0; column < boardProportions; column++) {
+                            const squareId = [getColumnLetter(column, boardProportions), getRowNumber(row, boardProportions)]
 
                             let bgColor, guideColor
                             if (isEven(row)) {
@@ -47,15 +52,15 @@ export default function Board(props: { boardSize: number, boardColors: string[] 
 
                             // text-whiteBoard / text-blackBoard
                             let squareNumGuide, squareLetterGuide
-                            if (row === boardSize - 1) {
-                                squareLetterGuide = <span className={`absolute bottom-0 right-2 text-${guideColor}`}>{squareId[0]}</span>
+                            if (row === boardProportions - 1) {
+                                squareLetterGuide = <span style={{right: rightSize}} className={`absolute bottom-0 text-${guideColor}`}>{squareId[0]}</span>
                             }
                             if (column === 0) {
-                                squareNumGuide = <span className={`absolute top-0 left-1 text-${guideColor}`}>{squareId[1]}</span>
+                                squareNumGuide = <span style={{left: leftSize}} className={`absolute top-0 text-${guideColor}`}>{squareId[1]}</span>
                             }
 
                             // bg-whiteBoard / bg-blackBoard
-                            squares.push(<div key={`${row}-${column}`} className={`bg-${bgColor} h-24 w-24 text-2xl font-bold select-none relative`}>{squareNumGuide}{squareLetterGuide}</div>)
+                            squares.push(<div key={`${row}-${column}`} style={{height: squareSize, width: squareSize, fontSize: guideSize}} className={`bg-${bgColor} font-bold select-none relative`}>{squareNumGuide}{squareLetterGuide}</div>)
                         }
                     }
                     return squares
