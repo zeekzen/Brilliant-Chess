50133b780f70b2332133ad9cb3f08faa214241b0
wDeloo <wdelo.hack@gmail.com>
2024-11-28 19:59:31 +0100

    board responsive

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -1,17 +1,43 @@
 "use client"
 
+import React, { useEffect, useState, useRef } from "react"
+
 import Board from "./board"
 import Clock from "./clock"
 
 const BOARD_PROPORTIONS = 8
 const BOARD_COLORS = ["whiteBoard", "blackBoard"]
-const BOARD_SIZE = 750
 
 export default function Game() {
+    const [boardSize, setBoardSize] = useState(750)
+
+    const componentRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        function updateBoardSize() {
+            const component = componentRef.current
+            const statusBar = component?.getElementsByTagName('div')[0]
+            
+            const componentHeight = component?.offsetHeight ?? 0
+            const statusBarHeight = statusBar?.offsetHeight ?? 0
+            const gapHeight = 9
+
+            const boardHeight = componentHeight - ((statusBarHeight * 2) + (gapHeight * 2))
+
+            setBoardSize(boardHeight)
+        }
+        
+        updateBoardSize()
+
+        window.addEventListener('resize', updateBoardSize)
+
+        return () => window.removeEventListener('resize', updateBoardSize)
+    }, [])
+
     return (
-        <div className="h-full flex flex-col gap-2">
+        <div ref={componentRef} className="h-full flex flex-col justify-between">
                 <div className="flex flex-row justify-between">-<Clock white={false} moving={false} time="10:00" /></div>
-                <Board boardProportions={BOARD_PROPORTIONS} boardColors={BOARD_COLORS} boardSize={BOARD_SIZE} />
+                <Board boardProportions={BOARD_PROPORTIONS} boardColors={BOARD_COLORS} boardSize={boardSize} />
                 <div className="flex flex-row justify-between">-<Clock white={true} moving={false} time="10:00" /></div>
         </div>
     )
