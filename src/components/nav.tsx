ba423a27bb828a59625c077a24e297adb0c4b724
wDeloo <wdelo.hack@gmail.com>
2024-11-30 12:46:53 +0100

    donate url linked to support me button

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -15,7 +15,7 @@ export default function Nav() {
                 <div className="flex flex-row gap-2 text-xl"><Image height={30} width={30} alt="logo" src="images/logo.svg" /><a href="/">Brilliant Chess</a></div>
             </div>
             <div className="flex flex-col *:px-3 *:py-2 *:transition-colors hover:*:bg-backgroundBoxHover text-sm *:text-foregroundGrey hover:*:text-foregroundHighlighted font-bold">
-                <a href="#" onMouseEnter={() => setSupportHover(true)} onMouseLeave={() => setSupportHover(false)} className="flex flex-row gap-2"><Heart class={`${supportHover ? "fill-foregroundHighlighted" : "fill-foregroundGrey"} transition-colors`} />Support Me</a>
+                <a href="https://www.paypal.com/donate/?hosted_button_id=S8SWJBNYZ2WFW" onMouseEnter={() => setSupportHover(true)} onMouseLeave={() => setSupportHover(false)} className="flex flex-row gap-2"><Heart class={`${supportHover ? "fill-foregroundHighlighted" : "fill-foregroundGrey"} transition-colors`} />Support Me</a>
                 <a href="https://github.com/wdeloo/Brilliant-Chess" onMouseEnter={() => setSrcHover(true)} onMouseLeave={() => setSrcHover(false)} className="flex flex-row gap-2"><GitHub class={`${srcHover ? "fill-foregroundHighlighted" : "fill-foregroundGrey"} transition-colors`} />Source Code</a>
             </div>
         </nav>
