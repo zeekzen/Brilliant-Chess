3eb1ed282ea39052d6f90f8b7c25a9972826d6ed
wDeloo <wdelo.hack@gmail.com>
2025-01-17 20:52:50 +0100

    delete useless export

--- src/components/menu/analyze/selectChessCom.tsx
+++ src/components/menu/analyze/selectChessCom.tsx
@@ -36,7 +36,7 @@ interface Game {
     eco: string
 }
 
-export const PLAYER_URL = 'https://www.chess.com/member/'
+const PLAYER_URL = 'https://www.chess.com/member/'
 
 function getMonthName(month: number) {
     switch (month) {
