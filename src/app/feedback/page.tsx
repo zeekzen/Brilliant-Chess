827aa997a7492955b443c835aae551f693fc00d3
wdeloo <wdelo.hack@gmail.com>
2025-01-27 18:40:56 +0100

    feedback page (not working yet)

---
+++ src/app/feedback/page.tsx
@@ -0,0 +1,55 @@
+import Chats from "@/components/svg/chats"
+
+function Required({ spaceLeft, color }: {spaceLeft: number, color: string}) {
+    return (
+        <span style={{marginLeft: spaceLeft, color }} className="text-lg font-extrabold">*</span>
+    )
+}
+
+function Guide() {
+    return (
+        <div className="py-2">
+            <div className="flex flex-row gap-2 text-lg font-bold">
+                <Required spaceLeft={0} color="crimson" />
+                Required
+            </div>
+            <div className="flex flex-row gap-2 text-lg font-bold">
+                <Required spaceLeft={0} color="aqua" />
+                Optional <span className="font-normal">(to be notified when the bug is fixed)</span>
+            </div>
+        </div>
+    )
+}
+
+export default function Feedback() {
+    return (
+        <div className="flex-grow h-full flex flex-col">
+            <h1 className="text-4xl font-extrabold mx-auto mt-6 flex flex-row items-center gap-4 w-fit"><Chats size={35} class="fill-foreground" />Feedback</h1>
+            <div className="flex-grow pt-14 p-8">
+                <form className="max-w-[800px] w-full bg-backgroundBox rounded-borderRoundness m-auto p-6 flex flex-col gap-3">
+                    <h2 className="text-2xl font-extrabold">Report a Bug</h2>
+                    <Guide />
+                    <div className="flex flex-col gap-2">
+                        <div>
+                            <label className="font-bold text-lg" htmlFor="text">Name<Required spaceLeft={2} color="aqua" /></label>
+                            <input name="name" className="flex h-7 max-w-[25ch] w-full items-center transition-colors px-1 rounded-borderRoundness border-border hover:border-borderHighlighted focus:border-borderHighlighted border-solid border-[1px] bg-backgroundBoxBox outline-none" id="text" type="text" />
+                        </div>
+                        <div>
+                            <label className="font-bold text-lg" htmlFor="email">Email<Required spaceLeft={2} color="aqua" /></label>
+                            <input name="email" className="flex h-7 max-w-[30ch] w-full items-center transition-colors px-1 rounded-borderRoundness border-border hover:border-borderHighlighted focus:border-borderHighlighted border-solid border-[1px] bg-backgroundBoxBox outline-none" id="email" type="email" />
+                        </div>
+                        <div>
+                            <label className="font-bold text-lg" htmlFor="description">Description<Required spaceLeft={2} color="crimson" /></label>
+                            <textarea name="description" rows={3} className="flex max-w-[50ch] w-full items-center transition-colors px-1 rounded-borderRoundness border-border hover:border-borderHighlighted focus:border-borderHighlighted border-solid border-[1px] bg-backgroundBoxBox outline-none resize-none" id="description" />
+                        </div>
+                        <div>
+                            <label className="font-bold text-lg" htmlFor="game">Game URL/PGN</label>
+                            <textarea name="game" rows={1} className="flex h-7 max-w-[45ch] w-full items-center transition-colors px-1 rounded-borderRoundness border-border hover:border-borderHighlighted focus:border-borderHighlighted border-solid border-[1px] bg-backgroundBoxBox outline-none resize-none" id="game" />
+                        </div>
+                        <input type="submit" className="w-fit h-fit py-1 px-2 mt-2 cursor-pointer rounded-borderRoundness text-lg bg-backgroundBoxBoxHighlighted hover:bg-backgroundBoxBoxHighlightedHover transition-all font-extrabold hover:shadow-shadowBoxBoxHighlighted" value="Send" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/svg/chats.tsx
@@ -0,0 +1,8 @@
+export default function Chats(props: { class: string, size: number }) {
+    return (
+        <svg className={props.class} width={props.size} height={props.size} viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 1H4V11H8L10 13L12 11H16V1Z" />
+            <path d="M2 5V13H7.17157L8.70711 14.5355L7.29289 15.9497L6.34315 15H0V5H2Z" />
+        </svg>
+    )
+}
