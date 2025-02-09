df21bea622365c8b515b6fda72f638e3b0552c63
wdeloo <wdelo.hack@gmail.com>
2025-02-09 19:15:55 +0100

    hoverdragsquare rounded

--- src/components/game/board.tsx
+++ src/components/game/board.tsx
@@ -744,7 +744,7 @@ export default function Board(props: { cleanArrows: () => void, controller: Cont
                                 />
                             }
 
-                            const hoverDragSquare = <div style={{ display: drag.is ? '' : 'none',  opacity: hoverDrag === squareId ? '100' : '', borderWidth: squareSize * 0.05 }} onMouseEnter={() => setHoverDrag(squareId)} onMouseLeave={() => setHoverDrag('')} className="absolute top-0 z-[30] left-0 w-full h-full border-opacity-65 opacity-0 block border-white pointer-events-auto" />
+                            const hoverDragSquare = <div style={{ display: drag.is ? '' : 'none',  opacity: hoverDrag === squareId ? '100' : '', borderWidth: squareSize * 0.05 }} onMouseEnter={() => setHoverDrag(squareId)} onMouseLeave={() => setHoverDrag('')} className={`absolute top-0 z-[30] left-0 w-full h-full border-opacity-65 opacity-0 block border-white pointer-events-auto ${rounded}`} />
 
                             const legalMove = legalMoves.includes(squareId as Square) ? (
                                 piece ?
