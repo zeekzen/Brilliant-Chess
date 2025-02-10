d725f6acdd941c2dc6bb60b603debbb2a070a582
wdeloo <wdelo.hack@gmail.com>
2025-02-10 22:00:39 +0100

    lichess sounds included in licenses

--- src/app/licenses/page.tsx
+++ src/app/licenses/page.tsx
@@ -7,7 +7,7 @@ export default function Licenses() {
             <header>
                 <h1 className="text-4xl font-extrabold mx-auto mt-6 flex flex-row items-center gap-4 w-fit"><License size={35} class="fill-foreground" />Licenses</h1>
             </header>
-            <main className="grid grid-cols-3 p-14">
+            <main className="grid grid-cols-3 p-14 gap-6">
                 <section className="bg-backgroundBox p-6 w-full rounded-borderRoundness">
                     <h1 className="text-2xl font-bold flex flex-row items-center mx-auto mb-8 w-fit gap-2">
                         <div className="flex flex-row">
@@ -17,8 +17,20 @@ export default function Licenses() {
                         Chess Pieces Images
                     </h1>
                     <ul className="text-lg">
-                        <li>Created by: <a className="text-blue-600 hover:underline font-bold" href="https://en.wikipedia.org/wiki/User:Cburnett">Cburnett</a></li>
-                        <li>License: <a className="text-blue-600 hover:underline font-bold" href="https://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License</a></li>
+                        <li>Created by: <a target="_blank" className="text-blue-600 hover:underline font-bold" href="https://en.wikipedia.org/wiki/User:Cburnett">Cburnett</a></li>
+                        <li>License: <a target="_blank" className="text-blue-600 hover:underline font-bold" href="https://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License</a></li>
+                    </ul>
+                </section>
+                <section className="bg-backgroundBox p-6 w-full rounded-borderRoundness">
+                    <h1 className="text-2xl font-bold flex flex-row items-center mx-auto mb-8 w-fit gap-2">
+                        <div className="flex flex-row mr-2">
+                            <Image className="rotate-[-20deg]" alt="king" src="/images/sound.svg" width={50} height={0} />
+                        </div>
+                        Board Sounds
+                    </h1>
+                    <ul className="text-lg">
+                        <li>Created by: <a target="_blank" className="text-blue-600 hover:underline font-bold" href="https://github.com/lichess-org">Lichess</a></li>
+                        <li>License: <a target="_blank" className="text-blue-600 hover:underline font-bold" href="https://www.gnu.org/licenses/agpl-3.0.html">GNU Affero General Public License</a></li>
                     </ul>
                 </section>
             </main>
