84d3e36763f46f24ebca8fc67cf59739972044b0
wdeloo <wdelo.hack@gmail.com>
2025-02-15 11:38:49 +0100

    robots: allow all

--- robots.ts
+++ robots.ts
@@ -4,6 +4,7 @@ export default function Robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: '*',
+            allow: '/',
             disallow: '/licenses/',
         },
         sitemap: 'https://brilliant-chess.com/sitemap.xml',
