a7629cc45556bd1c4c1e8f55d1b65ee88044c5ae
wdeloo <wdelo.hack@gmail.com>
2025-02-15 23:38:05 +0100

    simple pieces moved to svg components

--- src/components/game/capturedPieces.tsx
+++ src/components/game/capturedPieces.tsx
@@ -1,6 +1,6 @@
-import { BISHOP, KNIGHT, PAWN, PieceSymbol, QUEEN, ROOK } from "chess.js";
-import Image from "next/image";
+import { BISHOP, BLACK, KNIGHT, PAWN, PieceSymbol, QUEEN, ROOK, WHITE } from "chess.js";
 import { maxVertical } from "../../../tailwind.config";
+import SimplePieceSVG from "../svg/simplePiece";
 
 const PIECES_IMAGES = {
     w: {
@@ -19,7 +19,7 @@ const PIECES_IMAGES = {
     }
 }
 
-const PIECES_ORDER = [ PAWN, BISHOP, KNIGHT, ROOK, QUEEN ]
+const PIECES_ORDER: PieceSymbol[] = [ PAWN, BISHOP, KNIGHT, ROOK, QUEEN ]
 
 function formatAdvantage(advantage: number) {
     return advantage > 0 ? `+${advantage}` : ''
@@ -36,7 +36,7 @@ export default function CapturedPieces(props: { white: boolean, pieces: PieceSym
     return (
         <div className="flex flex-row h-full items-end vertical:gap-2 gap-1">
             <div className="w-fit h-full flex flex-row ml-[-3px]">
-                {Object.entries(groupedPieces).sort((a, b) => PIECES_ORDER.indexOf(a[0]) - PIECES_ORDER.indexOf(b[0])).map(([piece, count], i) => {
+                {Object.entries(groupedPieces).sort((a, b) => PIECES_ORDER.indexOf(a[0] as PieceSymbol) - PIECES_ORDER.indexOf(b[0] as PieceSymbol)).map(([piece, count], i) => {
                     const src = `/images/simplePieces/${PIECES_IMAGES[!white ? 'w' : 'b' as keyof object][piece as keyof object]}`
                     const outlineColor = "var(--border)"
                     const smallSize = window.innerWidth < maxVertical ? 8 : 15
@@ -45,14 +45,14 @@ export default function CapturedPieces(props: { white: boolean, pieces: PieceSym
                     return (
                         <div className="mb-[1px] flex flex-row items-end" key={i}>
                             {Array.from({ length: count }).map((_, i) => {
-                                return <Image
+                                return <SimplePieceSVG
                                     key={i}
+                                    piece={piece as PieceSymbol}
+                                    color={white ? WHITE : BLACK}
+                                    size={piece === PAWN ? smallSize : piece === ROOK || piece === KNIGHT || piece === BISHOP ? mediumSize : bigSize}
                                     className={`${piece === PAWN ? 'mx-[2px]' : piece === ROOK || piece === KNIGHT || piece === BISHOP ? 'mx-[1px]' : ''} ${i !== 0 ? 'vertical:ml-[-10px] ml-[-7px]' : ''}`}
-                                    priority alt={piece}
-                                    width={piece === PAWN ? smallSize : piece === ROOK || piece === KNIGHT || piece === BISHOP ? mediumSize : bigSize}
-                                    height={piece === PAWN ? smallSize : piece === ROOK || piece === KNIGHT || piece === BISHOP ? mediumSize : bigSize}
+                                    draggable
                                     style={{filter: `drop-shadow(1px 0 0 ${outlineColor}) drop-shadow(-1px 0 0 ${outlineColor}) drop-shadow(0 1px 0 ${outlineColor}) drop-shadow(0 -1px 0 ${outlineColor})`}}
-                                    src={src}
                                 />
                             })}
                         </div>
