e2554833e6163813be71b258e707e326293d34a4
wdeloo <wdelo.hack@gmail.com>
2025-03-01 12:09:08 +0100

    timer of troubles loading stockfish increased

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -90,7 +90,7 @@ export default function Game() {
         const threads = navigator.hardwareConcurrency ?? 1
         const hash = Math.floor(getAproxMemory() / 4)
 
-        const errorTimeout = setTimeout(() => pushPageError(setErrors, 'The browser is having some troubles loading Stockfish', "If the app doesn't work properly try restarting the browser."), 10000);
+        const errorTimeout = setTimeout(() => pushPageError(setErrors, 'The browser is having some troubles loading Stockfish', "If the app doesn't work properly try restarting the browser."), 15000);
         (async () => {
             await prepareStockfish(stockfish, threads, hash)
             clearTimeout(errorTimeout)
