f7c2ceb54fed42c76986c92e11bf4b64a011bd8f
wDeloo <wdelo.hack@gmail.com>
2025-05-19 00:59:46 +0200

    fixed missing cleanup funcition useeffect

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -65,6 +65,8 @@ export default function Nav() {
         }
 
         document.addEventListener('mousedown', handleClick)
+
+        return () => document.removeEventListener('mousedown', handleClick)
     }, [])
 
     return (
